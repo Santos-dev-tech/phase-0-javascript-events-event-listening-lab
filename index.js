dc1e26120b39bae20a739be0d2c7e0eee5f3d10b
santos <zainsantos21@gmail.com>
2025-03-23 15:08:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const input = document.getElementById("button"); 
+  
+    if (input) { 
+      input.addEventListener("click", () => alert("I was clicked"));
+    } else {
+      console.error("Element with ID 'button' not found.");
+    }
+  }
